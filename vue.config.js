768cf5c7b25554037c630fb305c7ccaf1820a9ef
lemonsong <1979875623@qq.com>
2021-07-29 15:51:08 +0800

    增加了husky git钩子

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
 module.export = {
   configureWebpack: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['url-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+        loader: 'babel-loader'
+      }
+    ],
     resolve: {
       extensions: ['.js', '.vue', 'json'],
       alias: {
